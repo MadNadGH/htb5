87b1a8ef0e165ee718d01fcfa6d0480a7f342b72
MadNadGH <toycatbodya@gmail.com>
2025-01-28 12:01:55 +0300

    Add files via upload

---
+++ ConsoleApp5.21/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5._21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 4, b = 8, c = 321;
+
+            Class16 class16 = new Class16(a, b, c);
+            Console.WriteLine(class16.Answer());
+           
+        }
+    }
+}
